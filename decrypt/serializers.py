025c2e474dfe81ce743c1462f630d09616180ce5
nlrdev <neale.bus@gmail.com>
2022-02-09 00:05:42 +0200

    seems to work

--- decrypt/serializers.py
+++ decrypt/serializers.py
@@ -3,11 +3,11 @@ import gnupg
 
 class DecryptMessageSerializer(serializers.Serializer):
     passphrase = serializers.CharField(max_length=1024)
-    encrypted_data = serializers.CharField(max_length=1024)
+    message = serializers.CharField(max_length=1024)
     
-    def create(self, validated_data):
-        passphrase = validated_data.pop("passphrase", None)
-        encrypted_data = validated_data.pop("encrypted_data", None)
+    def decrypted_data(self):
+        passphrase = self.validated_data['passphrase']
+        encrypted_data = self.validated_data['message']
         gpg = gnupg.GPG()
         decrypted_data = gpg.decrypt(encrypted_data, passphrase=passphrase)
         data = {
